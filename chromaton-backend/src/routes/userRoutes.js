ebcd88ceb7e6ad1247fc65227aaffb8c6d6b7e1a
AstronaveCometa <sebastian.leon.castillo@gmail.com>
2026-04-10 14:52:28 -0400

    middleware verificarFirebaseToken

--- chromaton-backend/src/routes/userRoutes.js
+++ chromaton-backend/src/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { registerUser, getUserByIdController } from "../controllers/usersController.js";
+import { verificarFirebaseToken } from "../middleware/authMiddleware.js";
 
 const router = Router();
 
-router.post("/users", registerUser);
-router.get("/users/:user_id", getUserByIdController);
+router.post("/users", verificarFirebaseToken, registerUser);
+router.get("/users/:user_id", verificarFirebaseToken, getUserByIdController);
 
 
 export default router;
